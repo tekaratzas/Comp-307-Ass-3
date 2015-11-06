afa12476c9409b3367c922c511f85c98d51ff012
Tommy Luo <Tommy_Creatine@wpa145034.wireless.mcgill.ca>
2015-11-05 19:55:33 -0500

    Login script finished

--- Login.php
+++ Login.php
@@ -12,6 +12,8 @@
 	//$servername = 'localhost';
 	$DBuser = 'root';
 	$DBpass = 'root';
+	$username = '';
+	$password = '';
 
 	/* localhost doesn't work for mac, 
 	I am leaving it for the TA to update the port 
@@ -31,16 +33,37 @@
 		$result = $link->query($sql);
 	}
 
+	/*same function as Caesar in the client side,
+	I just translated it to PHP. */
+	function Decrypt($string, $amount){
+		if($amount < 0)
+			return Decrypt($string, $amount + 26);
+		$decrypted = ''; 
+		for( $i=0; $i < strlen($string); $i++){
+			$code = ord($string[$i]);
+			if ( $code >= 65 && $code <= 90 )
+				$tmp = (( ($code - 65 + $amount) % 26) + 65);
+			else if( $code >= 97 && $code <= 122 )
+				$tmp = (( ($code - 97 + $amount) % 26) + 97);
+		}
+		return $decrypted;
+	}
+
+
+
 	if ( $result->num_rows > 0 ){
 		while($row = $result->fetch_assoc() ){
 			echo "member ID: " .$row["member_ID"]. "shared_key: " .$row["shared_key"]. "username: " .$row["username"]. "password: " .$row["password"];
+			if ( .$row["member_ID"] == $username && .$row["password"] == $password ){
+				echo "login successfully";
+			} else {
+				echo "login failed";
+			}
 		}
 
 	} else{
 		print("can not select data from the table");
 		die(mysql_error());
 	}
-
-
 
 ?>
---
+++ README.txt
@@ -0,0 +1,3 @@
+I was given the permission to use a late day by Professor Vybihal. 
+
+Also, due to the amount of other assignments I have, I simply can't finish the Logout.php. I tested every piece of code and file individually and they produce the desired result, I just can't conncet them to my Logout.php. If you have any other questions, please contact me. thank you 
